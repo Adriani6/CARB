e3c7818fa68d640cc74c7e623894336cf91ced4e
Adriani6 <adrian@adriani6.co.uk>
2016-02-12 22:41:04 +0000

    Fixed the code, converted to Javascript/JQuery.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-$(document).foundation();
+var api = {
+	client: "826248c693a84ebc95d876382280b089",
+	json : undefined
+}
+
+var functions = {
+
+	start: function(query){
+		var callback = "https://api.instagram.com/v1/tags/"+query+"/media/recent?client_id="+api.client;
+		return $.ajax({
+			type: 'POST',
+			url: callback,
+			dataType: "jsonp",
+			success: function(data){api.json = data.data;}
+		});
+	}
+	
+
+}
