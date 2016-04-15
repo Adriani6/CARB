6b11bf4cefbe0f6011f9744a64eacd91c733fd60
bobmac <bob@robertmcateer.com>
2016-04-15 16:29:20 +0100

    working on the database

---
+++ js/firebaseDatabase.js
@@ -0,0 +1,24 @@
+/**
+ * Created by bobmac on 15/04/2016.
+ */
+
+var storage = undefined;
+
+var userID = undefined;
+
+
+
+var Database = {
+    newDatabase: function(id){
+        userID = id;
+        storage = new Firebase("https://incandescent-inferno-6873.firebaseio.com/" + id);
+    },
+    addtoDatabase : function(tag){
+        storage.push({
+            id: userID,
+            terms: tag
+        });
+    }
+}
+
+Database.newDatabase(12);
